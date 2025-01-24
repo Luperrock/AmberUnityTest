15ba40f4c3b73b1e4da0f2d5dae762cfdbe6f4fa
Lupe Medina <luperrock@gmail.com>
2025-01-24 01:27:28 -0600

    First commit
    + Added project core

---
+++ Assets/GridMap/Scripts/Grid.cs
@@ -0,0 +1,173 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using Random = UnityEngine.Random;
+
+public class Grid {
+
+    public event EventHandler<OnGridValueChangedEventArgs> OnGridValueChanged;
+    public class OnGridValueChangedEventArgs : EventArgs {
+        public int x;
+        public int y;
+    }
+
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private int[,] gridArray;
+    private int playerX;
+    private int playerY;
+    private int pendingX;
+    private int pendingY;
+
+    public Grid(int width, int height, float cellSize, Vector3 originPosition) {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+
+        bool showDebug = true;
+        if (showDebug) {
+            TextMesh[,] debugTextArray = new TextMesh[width, height];
+
+            for (int x = 0; x < gridArray.GetLength(0); x++) {
+                for (int y = 0; y < gridArray.GetLength(1); y++) {
+                    debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 30, Color.white, TextAnchor.MiddleCenter);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                }
+            }
+            Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+            Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+            OnGridValueChanged += (object sender, OnGridValueChangedEventArgs eventArgs) => {
+                debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y].ToString();
+            };
+        }
+    }
+
+    public bool IsPositionWithinBounds(Vector3 Pos)
+    {
+        int x, y;
+        GetXY(Pos, out x, out y);
+        return x >= 0 && y >= 0 ;
+    }
+
+    public void GenerateRandomObstacles(int NumObstacles) {
+        for (int i = 0; i < NumObstacles; i++) {
+            SetValue(Random.Range(0, GetWidth() - 1), Random.Range(0, GetHeight() - 1), 1);
+        }
+    }
+
+    public void GetNextMove( out int outX, out int outY)
+    {
+        // Sample code, change me!
+        outX = playerX + 1;
+        outY = playerY + 1;
+    }
+
+    public void SetPlayerPosition(int inX, int inY)
+    {
+        SetValue(inX, inY, 2);
+        playerX = inX;
+        playerY = inY;
+    }
+    
+    public void SetPendingMove(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            SetValue(x, y, 3);
+            pendingX = x;
+            pendingY = y;
+        }
+    }
+
+    public void MovePlayer()
+    {
+        // Clear previous 
+        SetValue(playerX, playerY, 0);
+        SetPlayerPosition(pendingX, pendingY);
+    }
+    
+    public void ClearBoard()
+    {
+        for (int x = 0; x < gridArray.GetLength(0); x++) {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                if (gridArray[x, y] == 2 || gridArray[x, y] == 3)
+                    SetValue(x, y,  0);
+            }
+        }
+
+        playerX = playerY = -1;
+        pendingX = pendingY = -1;
+
+    }
+
+    public int GetWidth() {
+        return width;
+    }
+
+    public int GetHeight() {
+        return height;
+    }
+
+    public float GetCellSize() {
+        return cellSize;
+    }
+
+    public Vector3 GetWorldPosition(int x, int y) {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    public void GetXY(Vector3 worldPosition, out int x, out int y) {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value) {
+        if (x >= 0 && y >= 0 && x < width && y < height) {
+            gridArray[x, y] = value;
+            if (OnGridValueChanged != null) OnGridValueChanged(this, new OnGridValueChangedEventArgs { x = x, y = y });
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y) {
+        if (x >= 0 && y >= 0 && x < width && y < height) {
+            return gridArray[x, y];
+        } else {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+
+}
---
+++ Assets/GridMap/Scripts/Testing.cs
@@ -0,0 +1,122 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+// 0 Empty
+// 1 Obstacle
+// 2 Player
+// 3 Possible selection
+
+public class Testing : MonoBehaviour {
+
+    private Grid grid;
+    private bool isMovePending;
+
+    private void Start() {
+        grid = new Grid(10, 10, 5f, new Vector3(0, 0));
+        grid.GenerateRandomObstacles(20);
+        HeatMapVisual heatMapVisual = new HeatMapVisual(grid, GetComponent<MeshFilter>());
+    }
+
+    private void Update() {
+        HandleClickToModifyGrid();
+    }
+
+    private void HandleClickToModifyGrid() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 MousePosition = UtilsClass.GetMouseWorldPosition();
+            if (grid.IsPositionWithinBounds(MousePosition))
+            {
+                int gridValue = grid.GetValue(MousePosition);
+                if (gridValue == 0)
+                {
+                    int x, y;
+                    grid.GetXY(MousePosition, out x, out y);
+                    grid.SetPlayerPosition(x,y);
+                }
+            }
+        }
+    }
+
+    public void OnNextPressed()
+    {
+        if (isMovePending)
+        {
+            isMovePending = false;
+            grid.MovePlayer();
+        }
+        else
+        {
+            grid.GetNextMove(out var nextX, out var nextY);
+            grid.SetPendingMove(nextX, nextY);
+            isMovePending = true;
+        }
+    }
+
+    public void OnClearPressed()
+    {
+        isMovePending = false;
+        grid.ClearBoard();
+    }
+
+
+    private class HeatMapVisual {
+
+        private Grid grid;
+        private Mesh mesh;
+
+        public HeatMapVisual(Grid grid, MeshFilter meshFilter) {
+            this.grid = grid;
+            
+            mesh = new Mesh();
+            meshFilter.mesh = mesh;
+
+            UpdateHeatMapVisual();
+
+            grid.OnGridValueChanged += Grid_OnGridValueChanged;
+        }
+
+        private void Grid_OnGridValueChanged(object sender, System.EventArgs e) {
+            UpdateHeatMapVisual();
+        }
+
+        public void UpdateHeatMapVisual() {
+            Vector3[] vertices;
+            Vector2[] uv;
+            int[] triangles;
+
+            MeshUtils.CreateEmptyMeshArrays(grid.GetWidth() * grid.GetHeight(), out vertices, out uv, out triangles);
+
+            for (int x = 0; x < grid.GetWidth(); x++) {
+                for (int y = 0; y < grid.GetHeight(); y++) {
+                    int index = x * grid.GetHeight() + y;
+                    Vector3 baseSize = new Vector3(1, 1) * grid.GetCellSize();
+                    int gridValue = grid.GetValue(x, y);
+                    int maxGridValue = 4;
+                    float gridValueNormalized = Mathf.Clamp01((float)gridValue / maxGridValue);
+                    Vector2 gridCellUV = new Vector2(gridValueNormalized, 0f);
+                    MeshUtils.AddToMeshArrays(vertices, uv, triangles, index, grid.GetWorldPosition(x, y) + baseSize * .5f, 0f, baseSize, gridCellUV, gridCellUV);
+                }
+            }
+
+            mesh.vertices = vertices;
+            mesh.uv = uv;
+            mesh.triangles = triangles;
+        }
+
+    }
+}
